e4e135c761163e0c3d8200796d89ba4fd20222ce
Xirus2112 <66229925+Xirus2112@users.noreply.github.com>
2021-09-16 21:59:41 -0500

    Add files via upload
    Se actualiza debido a múltiples fallas, una vez corregido se sube la versión 2

--- assets/js/jobs.js
+++ assets/js/jobs.js
@@ -1,57 +1,52 @@
 const $video = document.querySelector('#video')
-    const $play = document.querySelector('#play')
-    const $pause = document.querySelector('#pause')
-    const $backward = document.querySelector('#backward')
-    const $forward = document.querySelector('#forward')
-
-    $play.addEventListener('click',handlePlay)
-    $pause.addEventListener('click',handlePause)
-    $backward.addEventListener('click', handleBackward)
-    $forward.addEventListener('click',handleForward)
-
-
-    function handlePlay(){
-        $video.play()
-        $play.hidden = true
-        $pause.hidden = false
-        console.log('Le di clic en Play')
-    }
-
-    function handlePause(){
-        $video.pause()
-        $pause.hidden = true
-        $play.hidden = false
-        console.log('Le di clic en pause')
-    }
-
-    function handleBackward(){
-        $video.currentTime = $video.currentTime - 10
-        
-        console.log('Retrocede 10s -> ', $video.currentTime)
-    }
-
-    function handleForward(){
-        // $video.currentTime = $video.currentTime + 10
-        $video.currentTime += 10
-
-        console.log('Avanza 10s -> ', $video.currentTime)
-    }
-
+const $play = document.querySelector('#play')
+const $pause = document.querySelector('#pause')
+const $backward = document.querySelector('#backward')
+const $forward = document.querySelector('#forward')
 const $range = document.querySelector('#range')
-$video.addEventListener('loadedmetadata',handleLoaded)
-$video.addEventListener('timeupdate',handleTimeUpdate)
 
-function handleLoaded(){
+$play.addEventListener('click', handlePlay)
+$pause.addEventListener('click', handlePause)
+$backward.addEventListener('click', handleBackward)
+$forward.addEventListener('click', handleForward)
+$video.addEventListener('loadedmetadata', handleLoaded)
+$video.addEventListener('timeupdate', handleTimeUpdate)
+$range.addEventListener('input', handleInput)
+
+
+function handlePlay() {
+    $video.play()
+    $play.hidden = true
+    $pause.hidden = false
+    console.log('Le di clic en Play')
+}
+
+function handlePause() {
+    $video.pause()
+    $pause.hidden = true
+    $play.hidden = false
+    
+}
+
+function handleBackward() {
+    $video.currentTime = $video.currentTime - 10
+}
+
+function handleForward() {
+    // $video.currentTime = $video.currentTime + 10
+    $video.currentTime += 10
+}
+
+function handleLoaded() {
     $range.max = $video.duration
-    console.log('Enpieza a recorer e range -> ', $video.duration)
+    
 }
 
-function handleTimeUpdate(){
-    // $range.value = $video.currentTime
-    // console.log('Tiempo actual -> ', $video.currentTime)
+function handleTimeUpdate() {
+    $range.value = $video.currentTime
 }
 
-$range.addEventListener('input',handleInput)
-function handleInput(){
+
+function handleInput() {
     $video.currentTime = $range.value
 }
